97295673ea4d7ccec1f0f8d24ed46161db1392b4
Aufa Rizky <aufarizky202@gmail.com>
2023-05-18 14:07:09 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/ayopintar/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ayopintar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+    }
+}
