3b77de94aa7d856e25765c728e5f8e0a0ba5477a
Kevin Alfito <kevinalfito69@gmail.com>
2023-05-20 09:35:46 +0700

    Add Login Fragment

--- app/src/main/java/com/example/ayopintar/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/example/ayopintar/ui/auth/login/LoginFragment.kt
@@ -1,4 +1,4 @@
-package com.example.ayopintar.ui.auth.register
+package com.example.ayopintar.ui.auth.login
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,28 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.ayopintar.databinding.FragmentRegisterBinding
+import com.example.ayopintar.R
+import com.example.ayopintar.databinding.FragmentLoginBinding
 
-class RegisterFragment : Fragment() {
-private var _binding : FragmentRegisterBinding? = null
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container, false)
         return binding.root
-
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnBack.setOnClickListener {
             it.findNavController().navigateUp()
+        }
+        binding.linkDaftar.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
     }
 
