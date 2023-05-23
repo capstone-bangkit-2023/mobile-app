79b06f2bfac7802e11f91996d583d817fee1de6a
Kevin Alfito <kevinalfito69@gmail.com>
2023-05-23 13:03:19 +0700

    Add bottom navigation fragment

--- app/src/main/java/com/example/ayopintar/ui/MainActivity.kt
+++ app/src/main/java/com/example/ayopintar/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.ayopintar.ui
 
-
 import android.os.Bundle
-import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.ayopintar.R
 import com.example.ayopintar.databinding.ActivityMainBinding
-
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,15 +16,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
-        supportActionBar?.hide()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        supportActionBar?.hide()
+        val bottomNavView: BottomNavigationView = binding.navBottomView
+        val navController = findNavController(R.id.nav_container)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_profesi,
+                R.id.navigation_tips,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavView.setupWithNavController(navController)
     }
 }
