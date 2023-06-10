9e896d6ff23328307a940a4c3394c85890cc4c80
Kevin Alfito <kevinalfito69@gmail.com>
2023-06-11 04:37:53 +0700

    Add Kuis Fragment

--- app/src/main/java/com/example/ayopintar/ui/dashboard/home/HomeFragment.kt
+++ app/src/main/java/com/example/ayopintar/ui/dashboard/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.ayopintar.ui.dashboard.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ayopintar.api.response.PelajaranResponse
 import com.example.ayopintar.databinding.FragmentHomeBinding
+import com.example.ayopintar.ui.kuis.KuisActivity
+import com.example.ayopintar.ui.kuis.KuisActivity.Companion.extraMapel
 
 
 class HomeFragment : Fragment() {
@@ -31,19 +34,52 @@ class HomeFragment : Fragment() {
     }
 
     private fun setAdapterSemuaKuis() {
-        binding.rvSemuaKuis.adapter = SemuaKuisAdapter(getListMapel())
+        val semuaKuisAdapter = SemuaKuisAdapter(getListMapel())
         binding.rvSemuaKuis.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvSemuaKuis.adapter = semuaKuisAdapter
+        semuaKuisAdapter.setOnItemClickCallback(object : SemuaKuisAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: PelajaranResponse) {
+                intentStartKuis(data)
+            }
+
+        })
+
 
     }
-    private fun setAdapterPoplerKuis(){
-        binding.rvKuisPopuler.adapter = PopulerKuisAdapter(getListMapel())
 
-        binding.rvKuisPopuler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+    private fun setAdapterPoplerKuis() {
+        val populerKuisAdapter = PopulerKuisAdapter(getListMapel())
+        binding.rvKuisPopuler.adapter = populerKuisAdapter
+        binding.rvKuisPopuler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        populerKuisAdapter.setOnItemClickCallback(object : PopulerKuisAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: PelajaranResponse) {
+                intentStartKuis(data)
+            }
+        })
     }
+    private fun intentStartKuis(data: PelajaranResponse){
+        val intent = Intent(requireActivity(), KuisActivity::class.java)
+            .putExtra(extraMapel, data.mataPelajaran)
+        startActivity(intent)
+    }
+
     private fun getListMapel(): ArrayList<PelajaranResponse> {
         val dataMapel = arrayOf("Bahasa Indonesia", "Penjas", "IPA", "IPS", "PKN")
-        val dataPendidikan = arrayOf("Sekolah Menengah Akhir", "Sekolah Menengah Kejuruan", "Sekolah Menengah Akhir", "Sekolah Menengah Akhir", "Sekolah Menengah Kejuuran")
-        val dataPhoto = arrayOf("https://siedoo.com/wp-content/uploads/2020/02/bahasa_indonesia2-1280x720-1-1210x642.jpg","https://source.unsplash.com/random/900×700/?city", "https://source.unsplash.com/random/900×700/?fruit", "https://source.unsplash.com/random/900×700/?sport", "https://source.unsplash.com/random/900×700/?vegetable")
+        val dataPendidikan = arrayOf(
+            "Sekolah Menengah Akhir",
+            "Sekolah Menengah Kejuruan",
+            "Sekolah Menengah Akhir",
+            "Sekolah Menengah Akhir",
+            "Sekolah Menengah Kejuuran"
+        )
+        val dataPhoto = arrayOf(
+            "https://siedoo.com/wp-content/uploads/2020/02/bahasa_indonesia2-1280x720-1-1210x642.jpg",
+            "https://source.unsplash.com/random/900×700/?city",
+            "https://source.unsplash.com/random/900×700/?fruit",
+            "https://source.unsplash.com/random/900×700/?sport",
+            "https://source.unsplash.com/random/900×700/?vegetable"
+        )
         val listHero = ArrayList<PelajaranResponse>()
 
         for (i in dataMapel.indices) {
@@ -52,6 +88,7 @@ class HomeFragment : Fragment() {
         }
         return listHero
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
