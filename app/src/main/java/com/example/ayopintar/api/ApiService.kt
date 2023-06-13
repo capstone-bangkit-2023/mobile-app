240b9d0788e1705ee44c5630f77154008f81d7a4
Aufa Rizky <aufarizky202@gmail.com>
2023-06-13 11:19:55 +0700

    Update ApiService

--- app/src/main/java/com/example/ayopintar/api/ApiService.kt
+++ app/src/main/java/com/example/ayopintar/api/ApiService.kt
@@ -1,4 +1,25 @@
 package com.example.ayopintar.api
 
+import com.example.ayopintar.api.response.LoginResponse
+import com.example.ayopintar.api.response.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+    @FormUrlEncoded
+    @POST("login")
+    fun loginApi(@Field("username") username: String, @Field("password") password: String) : Call<LoginResponse>
+
+    @FormUrlEncoded
+    @POST("register")
+    fun registerApi(
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("confirmPassword") confirmPassword: String,
+        @Field("nama") fullname: String,
+        @Field("namaSekolah") namaSekolah: String? = null,
+        @Field("email") email: String
+    ) : Call<RegisterResponse>
 }
