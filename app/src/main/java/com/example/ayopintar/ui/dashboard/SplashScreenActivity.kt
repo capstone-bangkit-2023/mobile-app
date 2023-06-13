63f9349220bc3729f7a9c581f327c3ab53656811
Aufa Rizky <aufarizky202@gmail.com>
2023-06-13 14:45:32 +0700

    Add logic to check token and decide where to go

--- app/src/main/java/com/example/ayopintar/ui/dashboard/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ayopintar/ui/dashboard/SplashScreenActivity.kt
@@ -1,14 +1,24 @@
 package com.example.ayopintar.ui.dashboard
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.example.ayopintar.R
+import com.example.ayopintar.token.TokenPreference
+import com.example.ayopintar.token.TokenViewModel
+import com.example.ayopintar.token.TokenViewModelFactory
 import com.example.ayopintar.ui.auth.AuthActivity
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
@@ -22,11 +32,20 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val pref = TokenPreference.getInstance(dataStore)
+        val tokenViewModel = ViewModelProvider(this, TokenViewModelFactory(pref))[TokenViewModel::class.java]
+
         Handler(Looper.getMainLooper()).postDelayed(
             {
-                val intent = Intent(this@SplashScreenActivity, AuthActivity::class.java)
-                startActivity(intent)
-                finish()
+                tokenViewModel.getToken().observe(this) {
+                    if (it != "") {
+                       startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        startActivity(Intent(this, AuthActivity::class.java))
+                        finish()
+                    }
+                }
             }, delay
         )
     }
