82ee441923d6e74015c04c69cbc31405a202a5e3
Aufa Rizky <aufarizky202@gmail.com>
2023-06-13 11:26:45 +0700

    Add GetListMapelResponse

---
+++ app/src/main/java/com/example/ayopintar/api/response/GetListMapelResponse.kt
@@ -0,0 +1,36 @@
+package com.example.ayopintar.api.response
+
+import com.google.gson.annotations.SerializedName
+
+data class GetListMapelResponse(
+
+	@field:SerializedName("code")
+	val code: Int,
+
+	@field:SerializedName("data")
+	val data: List<DataItem?>? = null,
+
+	@field:SerializedName("status")
+	val status: String
+)
+
+data class DataItem(
+
+	@field:SerializedName("link_foto")
+	val linkFoto: Any? = null,
+
+	@field:SerializedName("createdAt")
+	val createdAt: String? = null,
+
+	@field:SerializedName("kode_matapelajaran")
+	val kodeMatapelajaran: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("mata_pelajaran")
+	val mataPelajaran: String? = null,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String? = null
+)
