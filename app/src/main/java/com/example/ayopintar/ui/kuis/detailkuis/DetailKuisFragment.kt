0bab892c6ed1f89973e6eb865ba33e7e24637c2d
Aufa Rizky <aufarizky202@gmail.com>
2023-06-15 22:43:41 +0700

    Integrate DetailKuisFragment with DetailKuisViewModel and API stuff

--- app/src/main/java/com/example/ayopintar/ui/kuis/detailkuis/DetailKuisFragment.kt
+++ app/src/main/java/com/example/ayopintar/ui/kuis/detailkuis/DetailKuisFragment.kt
@@ -1,23 +1,53 @@
 package com.example.ayopintar.ui.kuis.detailkuis
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ayopintar.R
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.example.ayopintar.databinding.FragmentDetailKuisBinding
+import com.example.ayopintar.token.TokenPreference
+import com.example.ayopintar.token.TokenViewModel
+import com.example.ayopintar.token.TokenViewModelFactory
+import com.example.ayopintar.ui.kuis.KuisActivity
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
 
 class DetailKuisFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding : FragmentDetailKuisBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: DetailKuisViewModel
+    private lateinit var tokenViewModel: TokenViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_kuis, container, false)
+        _binding = FragmentDetailKuisBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val idMapel = requireActivity().intent.getStringExtra(KuisActivity.extraIdMapel)
+        val pref = TokenPreference.getInstance(requireContext().dataStore)
+        tokenViewModel = ViewModelProvider(this, TokenViewModelFactory(pref))[TokenViewModel::class.java]
+        viewModel = ViewModelProvider(this)[DetailKuisViewModel::class.java]
+
+        tokenViewModel.getToken().observe(this) {
+            viewModel.getSoal(it, idMapel!!)
+        }
+
+        viewModel.getSoalResult.observe(this) {
+            binding.tvSoal.text = it
+        }
     }
 }
--- app/src/main/java/com/example/ayopintar/ui/kuis/detailkuis/DetailKuisViewModel.kt
+++ app/src/main/java/com/example/ayopintar/ui/kuis/detailkuis/DetailKuisViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.ayopintar.ui.kuis.detailkuis
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.ayopintar.api.ApiConfig
+import com.example.ayopintar.api.response.GetSoalResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetailKuisViewModel : ViewModel() {
+
+    private val _getSoalResult = MutableLiveData<String>()
+    val getSoalResult: LiveData<String> = _getSoalResult
+
+    fun getSoal(token: String, id: String) {
+        val api = ApiConfig.getApiService().getSoal("Bearer $token", id)
+        api.enqueue(object : Callback<GetSoalResponse> {
+            override fun onResponse(call: Call<GetSoalResponse>, response: Response<GetSoalResponse>) {
+                if (response.isSuccessful) {
+                    if (response.body() != null) {
+                        _getSoalResult.value = response.body()!!.data.soal.soal
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<GetSoalResponse>, t: Throwable) {
+                Log.e("getSoal", "onFailure : ${t.message}")
+            }
+        })
+    }
 }
