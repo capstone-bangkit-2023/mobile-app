be43799f4d1e767e06c854f0ac3be40fc8d66b76
Aufa Rizky <aufarizky202@gmail.com>
2023-06-16 21:30:32 +0700

    Update ApiService

--- app/src/main/java/com/example/ayopintar/api/ApiService.kt
+++ app/src/main/java/com/example/ayopintar/api/ApiService.kt
@@ -42,4 +42,8 @@ interface ApiService {
         @Field("kode_soal") kodeSoal: String,
         @Field("jawaban") jawaban: String
     ) : Call<PostJawabanResponse>
+
+    @FormUrlEncoded
+    @POST("profile")
+    fun getprofile(@Header("Authorization") token: String, @Field("username") username: String) : Call<GetProfileResponse>
 }
